1cea3e146a80631af6bfa18a23960876652f38ef
valentinfaganel <valentin.faganel@pristop.si>
2015-12-29 20:53:08 +0100

    20151229 Initial commit

---
+++ testMVC/testMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testMVC.Startup))]
+namespace testMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
